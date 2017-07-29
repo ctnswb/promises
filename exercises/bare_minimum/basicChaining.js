da4a98da7ea8560b118c60c7c2538110835adc7a
ctnswb <xclai.07@gmail.com>
2017-07-29 14:36:05 -0700

    basic requirements

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -11,10 +11,37 @@
 var fs = require('fs');
 var Promise = require('bluebird');
 
+var promisification = require('./promisification.js');
+var promiseConstructors = require('./promiseConstructor.js');
+
+var getGitHubProfileAsync = promisification.getGitHubProfileAsync;
+var pluckFirstLineFromFileAsync = promiseConstructors.pluckFirstLineFromFileAsync;
+
+var writeState = function( profile, writeFilePath, callback){
+  fs.writeFile(writeFilePath, JSON.stringify(profile), 'utf8', function(err, content) {
+    if(err) {
+      return callback(err,null);
+    } else {
+      return callback(null, content);
+    }
+  });
+};
+
+var writeStateAsync = Promise.promisify(writeState);
 
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
-  // TODO
+  return pluckFirstLineFromFileAsync(readFilePath)
+    .then(function(user) {
+      return getGitHubProfileAsync(user);
+    })
+    .then(function(user){
+      return writeStateAsync(user, writeFilePath);
+    })
+    .catch(function(err) {
+      console.log(err.message);
+    });
+
 };
 
 // Export these functions so we can test them
